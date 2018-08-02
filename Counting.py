95516f2eb27805e47f2ce1a1f50152d065935991
Jerry Liu <alamod951753@gmail.com>
2018-08-02 15:26:56 +0800

    counting init commit

---
+++ Counting.py
@@ -0,0 +1,19 @@
+import collections
+
+urls = [
+"http://www.google.com/a.txt",
+"http://www.google.com.tw/a.txt",
+"http://www.google.com/download/c.jpg",
+"http://www.google.co.jp/a.txt",
+"http://www.google.com/b.txt",
+"https://facebook.com/movie/b.txt",
+"http://yahoo.com/123/000/c.jpg",
+"http://gliacloud.com/haha.png",
+]
+
+listFileName = []
+for url in urls:
+    listFileName.append(url[url.rindex('/')+1:])
+
+print collections.Counter(listFileName).most_common(3)
+
